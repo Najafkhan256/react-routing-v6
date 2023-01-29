47583d33a2290bf88bfa9dbc219a6934f2e38c47
Najaf <najafkhan6625@gmail.com>
2023-01-29 22:08:53 +0500

    react-router-dom v6

---
+++ src/Components/Routing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Routing = () => {
+  return (
+    <div className="nav-routing">
+      <div className="logo">
+        <h2>React Routing</h2>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Routing;
